9eab33d916ec010d16ffa9c4f7764c305d51aee9
NowfxWho <nowf.study@gmail.com>
2024-06-09 00:20:20 +0700

    Finish authentication page

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -39,7 +39,12 @@ const SignInForm = () => {
       );
       console.log(response);
       resetFormFields();
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+      if (error.code === "auth/invalid-credential") {
+        alert("Invalid credential");
+      }
+    }
   };
 
   const handleChange = (event) => {
@@ -71,7 +76,7 @@ const SignInForm = () => {
 
         <div className="buttons-container">
           <Button type="submit">Sign in</Button>
-          <Button buttonType="google" onClick={signInWithGoogle}>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
             Google sign in
           </Button>
         </div>
